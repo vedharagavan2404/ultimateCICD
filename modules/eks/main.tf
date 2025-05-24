0c602818f85c2904ff52b42fe3e257f99a05eff8
Ubuntu <ubuntu@ip-172-31-16-147.ca-central-1.compute.internal>
2025-05-24 14:43:56 +0000

    eks module fixed

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,87 +1,73 @@
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
 }
 
 resource "aws_eks_cluster" "main" {
-  name = var.cluster_name
-  version = var.cluster_version
+  name     = var.cluster_name
+  version  = var.cluster_version
   role_arn = aws_iam_role.cluster.arn
-  access_config {
-    authentication_mode = "API"
-  }
-  
 
   vpc_config {
     subnet_ids = var.subnet_ids
   }
 
-  # Ensure that IAM Role permissions are created before and deleted
-  # after EKS Cluster handling. Otherwise, EKS will not be able to
-  # properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.cluster_policy,
+    aws_iam_role_policy_attachment.cluster_policy
   ]
 }
 
-resource "aws_iam_role" "worker" {
-  # The IAM role for the EKS worker nodes.
-  name = "${var.cluster_name}-worker-role"
+resource "aws_iam_role" "node" {
+  name = "${var.cluster_name}-node-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "worker_policy" {
-  role       = aws_iam_role.worker.name
+resource "aws_iam_role_policy_attachment" "node_policy" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ])
-    policy_arn = each.value
+
+  policy_arn = each.value
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_eks_node_group" "main" {
   for_each = var.node_groups
 
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.cluster_name}-${each.key}-node-group"
-  node_role_arn       = aws_iam_role.worker.arn
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnet_ids
+
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
 
   scaling_config {
     desired_size = each.value.scaling_config.desired_size
@@ -89,10 +75,7 @@ resource "aws_eks_node_group" "main" {
     min_size     = each.value.scaling_config.min_size
   }
 
-  instance_types = each.value.instance_types
-  capacity_type = each.value.capacity_type
-
   depends_on = [
-    aws_iam_role_policy_attachment.worker_policy,
+    aws_iam_role_policy_attachment.node_policy
   ]
 }
